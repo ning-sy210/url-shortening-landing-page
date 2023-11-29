b1914e202b717a2c24d4c3db2b0e476e2e16abb7
Ning Sheng Ying <ning_sheng_ying@outlook.com>
2023-11-29 12:11:44 +0800

    config: setup project theme + Poppins font + fontawesome

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,33 @@ export default {
     files: ["./src/components/**/*.tsx"],
   },
   theme: {
-    extend: {},
+    screens: {
+      mobile: "375px",
+      desktop: "1440px",
+    },
+    fontFamily: {
+      sans: ["Poppins", "sans-serif"],
+    },
+    fontSize: {
+      body: "1.125rem", // 18px
+    },
+    fontWeight: {
+      medium: "500",
+      bold: "700",
+    },
+    extend: {
+      colors: {
+        "primary-1": "hsl(180, 66%, 49%)",
+        "primary-2": "hsl(257, 27%, 26%)",
+
+        "secondary-1": "hsl(0, 87%, 67%)",
+
+        "neutral-1": "hsl(0, 0%, 75%)",
+        "neutral-2": "hsl(257, 7%, 63%)",
+        "neutral-3": "hsl(255, 11%, 22%)",
+        "neutral-4": "hsl(260, 8%, 14%)",
+      },
+    },
   },
   plugins: [],
 };
