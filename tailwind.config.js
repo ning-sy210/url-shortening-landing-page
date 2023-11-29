39411ef2c2674b7877aaf2864eb022d1ae3e7f7b
Ning Sheng Ying <ning_sheng_ying@outlook.com>
2023-11-29 10:53:50 +0800

    config: setup tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: {
+    relative: true,
+    files: ["./src/components/**/*.tsx"],
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
