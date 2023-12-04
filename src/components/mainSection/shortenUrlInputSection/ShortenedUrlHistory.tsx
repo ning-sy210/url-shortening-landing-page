8612bca78700b67926a17abcff8079ad35ed6532
Ning Sheng Ying <ning_sheng_ying@outlook.com>
2023-12-04 18:35:59 +0800

    feat: add copy to clipboard function

--- src/components/mainSection/shortenUrlInputSection/ShortenedUrlHistory.tsx
+++ src/components/mainSection/shortenUrlInputSection/ShortenedUrlHistory.tsx
@@ -1,29 +1,42 @@
+import { useState } from "react";
+
 type ShortenedUrlHistoryType = {
   longUrl: string;
-  shortenedUrl?: string;
+  shortenedUrl: string;
 };
 
 const ShortenedUrlHistory = ({
   longUrl,
   shortenedUrl,
 }: ShortenedUrlHistoryType) => {
-  if (!shortenedUrl) return;
+  const [copied, setCopied] = useState(false);
+  const copiedStateClassnames = copied ? "bg-primary-2" : "";
+
+  function onCopyLinkClick() {
+    navigator.clipboard.writeText(shortenedUrl).then(() => {
+      setCopied(true);
+      setTimeout(() => {
+        setCopied(false);
+      }, 3000);
+    });
+  }
 
   return (
     <div className="content grid rounded text-start bg-white">
       <p
         title={longUrl}
-        className="px-4 pt-3 pb-2 border-b-[0.5px] border-solid border-neutral-1 text-default overflow-hidden text-ellipsis"
+        className="px-4 pt-3 pb-[0.375rem] border-b-[1px] border-solid border-gray-200 text-default overflow-hidden text-ellipsis"
       >
         {longUrl}
       </p>
-      <div className="px-4 pt-[0.625rem] pb-4 grid">
+      <div className="px-4 pt-3 pb-4 grid">
         <p className="text-default text-primary-1">{shortenedUrl}</p>
         <button
           type="button"
-          className="mt-2 cta-btn text-mobile-p-sm font-bold py-[0.4375rem] rounded-md"
+          onClick={onCopyLinkClick}
+          className={`mt-2 cta-btn text-mobile-p-sm font-bold py-[0.4375rem] rounded-md transition-colors duration-300 ${copiedStateClassnames}`}
         >
-          Copy
+          {copied ? "Copied!" : "Copy"}
         </button>
       </div>
     </div>
