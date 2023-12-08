01b1878d82cb03fbe9f15a6730f56fc6c5b9d86e
Ning Sheng Ying <ning_sheng_ying@outlook.com>
2023-12-08 10:27:39 +0800

    feat: make BoostCta responsive

--- src/components/BoostCta.tsx
+++ src/components/BoostCta.tsx
@@ -1,11 +1,13 @@
 const BoostCta = () => {
   return (
     // TODO: add background-image
-    <section className="full-width bg-primary-2 pt-[6.125rem] pb-[5.625rem]">
-      <h2 className="text-mobile-h2 text-white">Boost your links today</h2>
+    <section className="full-width bg-primary-2 pt-[6.125rem] pb-[5.625rem] desktop:pt-[4.375rem] desktop:pb-14">
+      <h2 className="text-mobile-h2 text-white desktop:text-[2.5rem] desktop:tracking-[-0.065rem] desktop:leading-[unset]">
+        Boost your links today
+      </h2>
       <button
         type="button"
-        className="cta-btn mt-4 px-10 py-[0.8125rem] rounded-full text-[1.25rem]"
+        className="cta-btn mt-4 px-10 py-[0.8125rem] rounded-full text-[1.25rem] desktop:mt-[2.3125rem]"
       >
         Get Started
       </button>
